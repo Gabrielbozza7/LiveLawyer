29787de1bbc25849f426e19d8fbcdf13a92db37a
TJH45-NJIT <tjh45@njit.edu>
2025-03-09 14:42:16 +0000

    feat: get basic functionality working with a new frontend server being able to connect to the backend socket room

--- LiveLawyerBackend/src/index.ts
+++ LiveLawyerBackend/src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 
 const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer)
+
 const port = 4000
 app.use(express.json())
 
@@ -8,7 +13,18 @@ app.get('/test', async (req, res) => {
   res.json({ it: 'works' })
 })
 
-app.listen(port, () => {
+app.get('/', async (req, res) => {
+  res.send(
+    '<!DOCTYPE html><html><body><script src="/socket.io/socket.io.js"></script><script>const socket = io();</script></body></html>',
+  )
+})
+
+io.on('connection', socket => {
+  console.log(`a user connected with id ${socket.id}`)
+  socket.join('testcall')
+})
+
+httpServer.listen(port, () => {
   console.log('Hi.')
   console.log(`Server is running on http://localhost:${port}`)
 })
