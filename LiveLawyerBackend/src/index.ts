85252a0a3396bb0abc141de45922f8502e1695f5
mrjigglys <djtheman007@gmail.com>
2025-03-15 16:17:33 +0000

    Implemented database select query.

--- LiveLawyerBackend/src/index.ts
+++ LiveLawyerBackend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import {supabase} from './database/supabase'
 
 const app = express()
 const port = 4000
@@ -6,6 +7,17 @@ app.use(express.json())
 
 app.get('/test', async (req, res) => {
   res.json({ it: 'works' })
+})
+
+app.get('/database', async (req, res) => {
+  try {  
+  let { data, error } = await supabase
+  .from('users')
+  .select('*')
+    res.send(data)
+  } catch (error) {
+    res.send(error)
+  }
 })
 
 app.listen(port, () => {
