47a89649274b584f8c3e08ada690447708f58159
TJH45-NJIT <tjh45@njit.edu>
2025-03-18 22:48:32 +0000

    chore: refactor video call management into a class

--- LiveLawyerWeb/src/app/page.tsx
+++ LiveLawyerWeb/src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client"
 import React, { useState, useEffect, useRef, useCallback} from 'react';
 import { socket } from './socket';
-import {startRoom} from './public/roomConnect';
+import TwilioVideoRoom from './public/TwilioVideoRoom';
 
 export default function App() {
   const formRef = useRef(null);
   const inputRef = useRef(null);
   const containerRef = useRef(null);
-  
+
+  const [videoRoom] = useState<TwilioVideoRoom>(new TwilioVideoRoom());
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [isConnected, setIsConnected] = useState(socket.connected);
 
@@ -29,10 +31,19 @@ export default function App() {
     };
   }, []);
 
-  const startRoomIntermediate = useCallback((event: React.FormEvent<HTMLFormElement>)=>{
-    startRoom(event,formRef.current!,containerRef.current!)
-    return false;
-  },[])
+  const joinRoom = useCallback(async (event: React.FormEvent<HTMLFormElement>) => {
+    const htmlForm: HTMLFormElement = formRef.current!;
+    const formData = new FormData(htmlForm)
+    const roomName = formData.get("room_name")!.toString()
+
+    // prevent a page reload when a user submits the form
+    event.preventDefault();
+    // hide the join form
+    htmlForm.style.visibility = "hidden";
+
+    console.log(await videoRoom.joinRoom(roomName));
+    videoRoom.setupListeners(containerRef.current!);
+  }, [videoRoom]);
 
   return(
     <div>
@@ -43,7 +54,7 @@ export default function App() {
         <script src="https://sdk.twilio.com/js/video/releases/2.15.2/twilio-video.min.js"></script>
       </div>
       <div>
-        <form id="room-name-form" ref={formRef} onSubmit={startRoomIntermediate}>
+        <form id="room-name-form" ref={formRef} onSubmit={joinRoom}>
           Enter a Room Name to join:
           <input ref={inputRef} name="room_name" id="room-name-input" />
           <button type="submit">Join Room</button>
---
+++ LiveLawyerWeb/src/app/public/TwilioVideoRoom.ts
@@ -0,0 +1,106 @@
+import { connect, Room, LocalParticipant, RemoteParticipant, LocalVideoTrack, LocalAudioTrack } from 'twilio-video'
+
+type mediaTrack = LocalAudioTrack | LocalVideoTrack;
+
+const handleConnectedParticipant = (participant: LocalParticipant | RemoteParticipant,containerRef:HTMLDivElement) => {
+  //div created
+  const participantDiv = document.createElement('div')
+  participantDiv.setAttribute("id",participant.identity)
+  containerRef.appendChild(participantDiv)
+
+  // iterate through the participant's published tracks and
+// call `handleTrackPublication` on them
+  participant.tracks.forEach(trackpublication=>{
+    console.log("trackPublication", trackpublication)
+    handleTrackPublication(trackpublication, participant);
+  })
+
+  participant.on("trackPublished", handleTrackPublication);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const handleTrackPublication = (trackPublication:any,participant: LocalParticipant | RemoteParticipant) => {
+  console.log("-----------------------\nIDENTITY: ",participant.identity)
+
+  function displayTrack(track:mediaTrack){
+    const participantDiv = document.getElementById(participant.identity);
+    if (track != null && track != undefined) {
+      participantDiv?.appendChild(track.attach())
+    }
+  }
+  // check if the trackPublication contains a `track` attribute. If it does,
+  // we are subscribed to this track. If not, we are not subscribed.
+  if(trackPublication.track){
+    displayTrack(trackPublication.track)
+    
+    
+  }
+  // listen for any new subscriptions to this track publication
+  trackPublication.on("subscribed", displayTrack);
+}
+
+const handleDisconnectedParticipant = (participant: LocalParticipant | RemoteParticipant) => {
+  participant.removeAllListeners();
+
+  const participantDiv = document.getElementById(participant.identity);
+  participantDiv?.remove()
+}
+
+
+export default class TwilioVideoRoom {
+    private token: string;
+    private room: Room | undefined;
+    
+    constructor() {
+      this.token = "";
+      this.room = undefined;
+    }
+
+    public async joinRoom(roomName: string): Promise<boolean> {
+      try {
+        const response = await fetch("http://localhost:4000/join-room", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({roomName: roomName}),
+          mode: 'cors'
+        });
+        const { token: retrievedToken } = await response.json();
+        this.token = retrievedToken;
+      } catch (error: unknown) {
+        console.log(`POST error: ${(error as Error).message}`)
+        return false;
+      }
+      try {
+        this.room = await connect(this.token, {
+          name: roomName,
+        });
+      } catch (error: unknown) {
+        console.log(`Able to get token, but could not join room: ${(error as Error).message}`);
+        return false;
+      }
+      return true;
+    }
+
+    public setupListeners(containerRef: HTMLDivElement) {
+      if (this.room === undefined) {
+        console.log("Cannot setup listeners if a room hasn't been joined!");
+        return;
+      }
+      // render the local and remote participants' video and audio tracks
+      handleConnectedParticipant(this.room.localParticipant,containerRef);
+      this.room.participants.forEach((participant) => {
+        handleConnectedParticipant(participant,containerRef)
+      });
+      this.room.on("participantConnected", (participant) => {
+        handleConnectedParticipant(participant,containerRef)
+      });
+
+      //disconnect issues
+      this.room.on("participantDisconnected", handleDisconnectedParticipant)
+      window.addEventListener("pagehide", () => this.room!.disconnect());
+      window.addEventListener("beforeunload", () => this.room!.disconnect());
+    }
+}
