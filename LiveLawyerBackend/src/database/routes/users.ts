5b3b23f8cf73f09b4d091aa6a781df09f5da9344
Ghulam Ahmed <noorghulam7153@gmail.com>
2025-04-08 23:28:41 +0000

    feat: Update Prisma Schema to use public and auth schema & update User API route

--- LiveLawyerBackend/src/database/routes/users.ts
+++ LiveLawyerBackend/src/database/routes/users.ts
@@ -4,29 +4,54 @@ import prisma from '../prisma'
 const router = express.Router()
 
 /**
- * Route which returns all Users in database table to allow for display on frontend
+ * GET ALL.
+ * Route: GET /users
+ * Fetch all users.
  */
 router.get('/', async (req, res) => {
     try {
         const users = await prisma.user.findMany()
-        res.json({ users })
-    } catch (err) {
-        console.error(err)
+        res.status(200).json({ message: 'Succesfully Fetched Users', users })
+        //console.log(users)
+    } catch (error) {
+        console.error(error)
         res.status(500).json({ error: 'Error Fetching Users' })
     }
 })
 
 /**
- * Route that allows for a creation of a user.
- * Usertype Param is not required since it is defaulted to Civilian upon creation.
- * ProfPicUrl param is also not required since it is optional for a user to have a profile pic.
+ * GET ONE.
+ * Route: GET /users/:id
+ * Fetches one user.
+ */
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id }
+        })
+        if (!user) {
+            res.status(404).json({ error: 'User not Found' })
+        }
+        res.status(200).json({ message: 'Succesfully Fetched User', user })
+        //console.log(user)
+    } catch (error) {
+        console.error(error)
+        res.status(404).json({ error: 'User Not Found or Does Not Exist' })
+    }
+})
+
+/**
+ * CREATE.
+ * Route: POST /users
+ * Creates a new user row.
  */
 router.post('/', async (req, res) => {
-    const { firstName, lastName, email, phoneNum, userType, profPicUrl } = req.body
-
+    const { id, firstName, lastName, email, phoneNum, userType, profPicUrl } = req.body
     try {
         const newUser = await prisma.user.create({
             data: {
+                id,
                 firstName,
                 lastName,
                 email,
@@ -35,17 +60,17 @@ router.post('/', async (req, res) => {
                 profPicUrl,
             },
         })
-        res.status(201).json(newUser)
-    } catch (err) {
-        console.error(err)
+        res.status(201).json({ message: 'Succesfully Created A New User: ', newUser })
+    } catch (error) {
+        console.error(error)
         res.status(400).json({ error: 'Failed to create user' })
     }
 })
 
 /**
- * Route that allows for a specific user in the Database Table to be updated.
- * Uses may include functionality where a user wants to update his own info.
- * Make sure row level security on the database allows for only a user to edit himself.
+ * UPDATE.
+ * Route: PUT /users/:id
+ * Updates an existing user by ID.
  */
 router.put('/:id', async (req, res) => {
     const { id } = req.params
@@ -62,12 +87,29 @@ router.put('/:id', async (req, res) => {
                 profPicUrl,
             },
         })
-        res.json(updatedUser)
-    } catch (err) {
-        console.error(err)
+        res.status(200).json({ message: 'Successfully Updated User', updatedUser })
+    } catch (error) {
+        console.error(error)
         res.status(404).json({ error: 'User not found or update failed' })
     }
 })
 
+/**
+ * DELETE.
+ * Route: DELETE /users/:id
+ * Deletes a User by ID.
+ */
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const deletedUser = await prisma.user.delete({
+            where: { id }
+        })
+        res.status(200).json({ message: 'User Deleted Succesfully', deletedUser })
+    } catch (error) {
+        console.error(error)
+        res.status(404).json({ error: 'User not found or deletion failed.' })
+    }
+})
 
 export default router
