2c9fa03b8d24d4cd1fb7d02190eae899abaa461f
TJH45-NJIT <tjh45@njit.edu>
2025-06-30 13:34:37 +0000

    chore: remove artificial delay when opening the app or signing in

--- LiveLawyerApp/app/index.tsx
+++ LiveLawyerApp/app/index.tsx
@@ -1,13 +1,22 @@
-import { useEffect } from 'react'
-import { useRouter } from 'expo-router'
+import { useEffect, useRef } from 'react'
+import { useRootNavigationState, useRouter } from 'expo-router'
 
 export default function Index() {
   const router = useRouter()
+  const rootNavigationState = useRootNavigationState()
+  const initialNavigatorState = useRef<string | undefined>(undefined)
+
+  // The logic here is supposed to make the router navigate to the main tabs screen
+  // when the router's root navigation state changes. If this check isn't performed,
+  // the app crashes.
 
   useEffect(() => {
-    ;(async () => {
-      await new Promise(resolve => setTimeout(resolve, 3000))
-      router.replace('/(tabs)')
-    })()
+    initialNavigatorState.current = rootNavigationState?.key
   }, [])
+
+  useEffect(() => {
+    if (rootNavigationState?.key !== initialNavigatorState.current) {
+      router.replace('/(tabs)')
+    }
+  }, [rootNavigationState?.key])
 }
